677c51813c766086a031c90078e42caeb5c61452
simondv4 <simondv4@gmail.com>
2024-01-05 10:12:25 +0300

    Пытаюсь сделать ветку мастер пустой

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-//
-public class Main {
-    public static void main(String[] args) {
-        String priceList = "";
-        priceList += Calc.getProduct();
-        System.out.println(priceList);
-    }
-}
